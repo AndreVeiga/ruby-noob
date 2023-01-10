7003010fa59e2172171158d829773a4d9e825556
AndreVeiga <soutomachado1@gmail.com>
2023-01-10 02:12:00 +0044

    ruby - aula 1

---
+++ hello.rb
@@ -0,0 +1,10 @@
+puts "Qual é o seu nome?"
+nome = gets.chomp
+
+puts "Qual é a sua idade?"
+idade = gets.chomp.to_i
+
+puts "hello #{nome}, e tem #{idade} anos."
+print "hello #{nome}, e tem #{idade} anos. \n"
+
+# comentários
