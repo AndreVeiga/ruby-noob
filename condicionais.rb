b8eff5a09dfb50d7ac39688ea84305b0e146fef8
AndreVeiga <soutomachado1@gmail.com>
2023-01-10 18:39:27 +0044

    Aula 4 Ruby

--- condicionais.rb
+++ condicionais.rb
@@ -4,10 +4,23 @@ idade = gets.chomp.to_i
 maiorIdade = 18
 melhorIdade = 65
 
+=begin
+
 if idade >= maiorIdade && idade < melhorIdade
   puts "Você é maior de idade."
 elsif idade >= melhorIdade  
   puts "Você esta na melhor idade."
 else
   puts "Você não é maior de idade."
+end
+
+=end
+
+case idade
+when 0..17 then
+  puts "Você não é maior de idade."
+when 18..64 then
+  puts "Você é maior de idade."
+else
+  puts "Você esta na melhor idade."
 end
